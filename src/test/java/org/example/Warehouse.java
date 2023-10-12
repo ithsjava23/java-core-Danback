1b69f2b6148b30088eec46f2724691c94bdbba38
Daniel <daniel.backberg@iths.se>
2023-10-12 17:56:19 +0200

    pc finished16

--- src/test/java/org/example/Warehouse.java
+++ src/test/java/org/example/Warehouse.java
@@ -8,7 +8,7 @@ public class Warehouse {
     private final Map<UUID, ProductRecord> products;
     private String name;
 
-    public Warehouse(String name) {
+    private Warehouse(String name) {
         this.name = name; // Set the name field
         this.products = new HashMap<>();
     }
