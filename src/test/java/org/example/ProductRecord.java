10e40309488d39b005dfa48d0858488e6c4b02cf
Daniel <daniel.backberg@iths.se>
2023-10-13 13:13:52 +0200

    pc finished29

--- src/test/java/org/example/ProductRecord.java
+++ src/test/java/org/example/ProductRecord.java
@@ -5,13 +5,15 @@ import java.util.UUID;
 
 public class ProductRecord {
     private final UUID id;
+    private final String name;
     private final Category category;
     private BigDecimal price;
     private BigDecimal previousPrice;
-    private boolean hasChanged; // Add a boolean flag to track changes
+    private boolean hasChanged;
 
     public ProductRecord(UUID id, String name, Category category, BigDecimal price) {
         this.id = id;
+        this.name = name;
         this.category = category;
         this.price = price;
         this.previousPrice = price;
@@ -25,7 +27,7 @@ public class ProductRecord {
     public void setPrice(BigDecimal price) {
         this.previousPrice = this.price;
         this.price = (price != null) ? price : BigDecimal.ZERO;
-        this.hasChanged = !this.price.equals(previousPrice); // Check for changes
+        this.hasChanged = !this.price.equals(previousPrice);
     }
 
     public UUID uuid() {
@@ -37,14 +39,26 @@ public class ProductRecord {
     }
 
     public boolean hasChanged() {
-        return hasChanged; // Return the change status
+        return hasChanged;
+    }
+
+    public String name() {
+        return name;
     }
 
     public Category category() {
-        return null;
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRecord{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", price=" + price +
+                ", previousPrice=" + previousPrice +
+                ", hasChanged=" + hasChanged +
+                '}';
     }
 }
-
-
-
-
